7482450b2655e78ec1965bb3fa58c737693fc63e
feyishola <feyisholaologunebi@gmail.com>
2023-11-06 22:22:17 +0100

    workde on app.js

--- src/app.js
+++ src/app.js
@@ -8,8 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const Port = process.env.PORT || 3000;
+
 app.use("api/v1/");
 
-app.listen(3000, () => {
+app.listen(Port, () => {
   console.log("app listening on port 3000");
 });
